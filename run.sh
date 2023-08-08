3ce05281c97627987ebd602b59a158215a38ff2e
Christian Marius NGUIEPE SEGNOU <50927207+ChrisDMI@users.noreply.github.com>
2023-08-09 01:20:12 +0200

    Update run.sh
    remove aws keys

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ docker run -it\
  -p 4000:4000\
  -v "$(pwd):/home/app"\
  -e APP_URI="https://chris-mlflow-tracking.herokuapp.com/"\
- -e AWS_ACCESS_KEY_ID="AKIA5PG57CCAVLA4JKVV"\
- -e AWS_SECRET_ACCESS_KEY="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"\
+ -e AWS_ACCESS_KEY_ID=""\
+ -e AWS_SECRET_ACCESS_KEY=""\
  chrisdmi/mlflowtracker python train.py
